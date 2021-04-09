6ec03e828e2e1ee6f707a1812fce78f7df4f9490
EC2 Default User <ec2-user@ip-172-31-2-96.ec2.internal>
2021-04-09 05:45:17 +0000

    Finish

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,41 +1,43 @@
 class TodolistsController < ApplicationController
   def new
-  @list=List.new
+    @list = List.new
   end
 
- def create
+  def create
+    list = List.new(list_params)
 
-  list = List.new(list_params)
+    list.save
 
-  list.save
+    redirect_to todolist_path(list.id)
+  end
 
-  redirect_to todolist_path(list.id)
+  def index
+    @lists = List.all
+  end
 
- end
- def index
-    @lists=List.all
- end
- def show
-     @list=List.find(params[:id])
- end
- def edit
-     @list = List.find(params[:id])
- end
- def update
-     list = List.find(params[:id])
-     list.update(list_params)
-     redirect_to todolist_path(list.id)
- end
- def destroy
-     list = List.find(params[:id])
-     list.destroy
-     redirect_to todolists_path
- end     
+  def show
+    @list = List.find(params[:id])
+  end
 
- private
+  def edit
+    @list = List.find(params[:id])
+  end
 
- def list_params
-   params.require(:list).permit(:title, :body, :image)
- end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
 end
